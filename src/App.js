9d6b4b2ebf58b92f29eba73c7edbf573438c7554
Faize Erturk <faizeeeturk@gmail.com>
2022-03-06 22:02:51 +0300

    update menu-items to navigate withRouter HOC
    -higher order component used
    -withRouter used
    -react-router-dom used

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <>
+    <h1>Hats Page</h1>
+  </>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch> 
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop/hats' component={HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
